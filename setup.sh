51626e9dcbd688203429ca8ae163a4ae3483353d
椎名深雪 <xiaochun.tong@mail.utoronto.ca>
2022-08-07 20:35:42 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 sudo apt update
 sudo apt install build-essentials git cmake clang
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
 cargo install sccache
 echo "export RUSTC_WRAPPER=sccache" >> ~/.bashrc
 source ~/.bashrc
