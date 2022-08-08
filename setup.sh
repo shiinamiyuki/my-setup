5d9ca8d6ada42eb6022e66e4d4e87218a0750759
椎名深雪 <xiaochun.tong@mail.utoronto.ca>
2022-08-08 19:49:03 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
+OLD=.old-bashrc
+if [ ! -f OLD ] then
+  mv ~/.bashrc $OLD
+fi
+
 sudo apt update
-sudo apt install gcc g++ cmake clang pkg-config libssl-dev
+sudo apt install gcc g++ cmake clang pkg-config libssl-dev python3-dev neovim python3-venv wget
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 sudo ./llvm.sh 14
@@ -7,4 +12,5 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
 cargo install sccache
 echo "export RUSTC_WRAPPER=sccache" >> ~/.bashrc
+echo "export vi=nvim" >> ~/.bashrc
 source ~/.bashrc
