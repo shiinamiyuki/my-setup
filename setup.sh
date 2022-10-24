79ddc8b62403d4c74eb5b07d14c1b07dc13cd3d3
椎名深雪 <xiaochun.tong@mail.utoronto.ca>
2022-10-24 19:11:01 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ if [ ! -f $OLD ] ;then
 fi
 
 sudo apt update
-sudo apt install gcc g++ cmake clang pkg-config libssl-dev python3-dev neovim python3-venv wget ninja-build libc++-dev libc++abi-dev
+sudo apt install curl gcc g++ cmake clang pkg-config libssl-dev python3-dev neovim python3-venv wget ninja-build libc++-dev libc++abi-dev libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libxext-dev libxfixes-dev libgl1-mesa-dev
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 sudo ./llvm.sh 14
