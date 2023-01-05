5b27e2f67556150fb94ad44500ffcd7eb999d643
椎名深雪 <xiaochun.tong@mail.utoronto.ca>
2023-01-05 17:09:04 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo apt update
 sudo apt install curl gcc g++ cmake clang pkg-config libssl-dev python3-dev neovim python3-venv wget ninja-build libc++-dev libc++abi-dev libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libxext-dev libxfixes-dev libgl1-mesa-dev
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
-sudo ./llvm.sh 14
+sudo ./llvm.sh 15
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
 cargo install sccache
